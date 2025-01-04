1a23841adfc581adc53e4ef7a42b148c486b5226
Dasha <dasha_petrova00@bk.ru>
2025-01-04 14:53:10 +0300

    php

--- index.php
+++ index.php
@@ -10,14 +10,14 @@
     <div>
         <h1 class="text-4xl font-bold text-blue-700">Авторизуйтесь!</h1>
         <?php
-       if (!isset($_COOKIE\['User'\])) {
-    ?>
-       <a href="/registration.php">Зарегистрируйтесь</a> или <a href="/login.php">войдите</a>, чтобы просматривать контент!
-    <?php
-       } else {
-           // подключение к БД
-       }
-    ?>
+        if (!isset($_COOKIE['User'])) {
+        ?>
+        <a href="/registration.php">Зарегистрируйтесь</a> или <a href="/login.php">войдите</a>, чтобы просматривать контент!
+        <?php
+            } else {
+                // подключение к БД
+            }
+        ?>
     </div>
 </body>
 </html>
