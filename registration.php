9915b3a122fb4df4de1dcffb4aae42c7e125395c
Dasha <dasha_petrova00@bk.ru>
2025-01-04 18:09:21 +0300

    Добавила регистрацию

--- registration.php
+++ registration.php
@@ -18,7 +18,7 @@
             </div>
             <div>
                 <label for="login" class="block text-sm font-medium text-gray-700">Логин</label>
-                <input type="text" id="login" name="login" required
+                <input type="text" id="username" name="username" required
                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500">
             </div>
             <div>
