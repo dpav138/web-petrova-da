76eb0b022809161e4fde4f01ce0f16518b12a5df
Dasha <dasha_petrova00@bk.ru>
2025-01-06 10:04:50 +0300

    Обновила index.php

--- index.php
+++ index.php
@@ -8,14 +8,25 @@
 </head>
 <body class="bg-gray-50 text-gray-800 font-sans">
     <div class="text-center mb-8">
-        <h1 class="text-4xl font-bold text-blue-700">Авторизуйтесь!</h1>
+        <h1 class="text-4xl font-bold text-blue-700">Посты пользователей</h1>
         <?php
         if (!isset($_COOKIE['User'])) {
         ?>
         <a class="text-bold text-blue-700" href="registration.php">Зарегистрируйтесь</a> или <a class="text-bold text-blue-700" href="login.php">войдите</a>, чтобы просматривать контент!
         <?php
             } else {
-                // подключение к БД
+                $link = mysqli_connect('127.0.0.1', 'root', 'your_password', 'nameDB');
+
+                $sql = "SELECT * FROM posts";
+                $res = mysqli_query($link, $sql);
+
+                if (mysqli_num_rows($res) >  0) {
+                    while ($post = mysqli_fetch_array($res)) {
+                        echo "<a href='/posts.php?id=" . $post["id"] . "'>" . $post['title'] . "</a>\n";
+                    }
+                } else {
+                    echo "Записей пока нет";
+                }
             }
         ?>
     </div>
